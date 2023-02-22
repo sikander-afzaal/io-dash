51396174a21c97e1cea256992466e30d3c8f4ad7
sikander-afzaal <sikander.aaa@gmail.com>
2023-02-22 19:45:21 +0500

    update

---
+++ src/layout/ContactLayout.jsx
@@ -0,0 +1,233 @@
+import { useState } from "react";
+
+const ContactLayout = ({ children }) => {
+  const [activeTab, setActiveTab] = useState("overview");
+  return (
+    <div className="w-full max-w-full flex flex-col overflow-x-hidden">
+      <div className="sticky h-max top-0 bg-white py-8 flex justify-start items-start flex-col w-full">
+        <div className="hide-scrollbar w-full border-b border-solid overflow-x-auto max-w-full border-[#E8ECEF]">
+          <div className="flex  min-w-max  max-w-full  pb-2 px-2 justify-start items-center gap-8  ">
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="overview"
+              ico={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+                  />
+                </svg>
+              }
+            />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="milestones"
+              ico={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                  />
+                </svg>
+              }
+            />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="panel"
+              ico={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+              }
+            />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="questionnaire"
+              ico={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                  />
+                </svg>
+              }
+            />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="answers"
+              ico={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                  />
+                </svg>
+              }
+            />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="share"
+              ico={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                  />
+                </svg>
+              }
+            />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="history"
+            />
+            <HeadTab active={activeTab} setActive={setActiveTab} name="files" />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="calendar"
+            />
+            <HeadTab active={activeTab} setActive={setActiveTab} name="deck" />
+            <HeadTab
+              active={activeTab}
+              setActive={setActiveTab}
+              name="permissions"
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-between gap-4 items-center w-full border-b border-solid border-[#E8ECEF] hide-scrollbar  px-2">
+          <input
+            type="text"
+            placeholder="Search for task and etc"
+            className="w-full max-w-[400px] border-r border-[#e8ecef] border-solid outline-none bg-transparent h-[50px] text-[#778CA2] text-base"
+          />
+          <div className="flex justify-center items-center gap-2">
+            <button className="w-[38px] grid place-items-center cursor-pointer hover:bg-[rgba(226,226,234,0.7)] transition-all aspect-square border border-solid border-[rgba(226,226,234,0.7)] rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#B8B9BE"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+                />
+              </svg>
+            </button>
+            <button className="w-[38px] grid place-items-center cursor-pointer hover:bg-[rgba(226,226,234,0.7)] transition-all aspect-square border border-solid border-[rgba(226,226,234,0.7)] rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#B8B9BE"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
+                />
+              </svg>
+            </button>
+            <button className="w-[38px] grid place-items-center cursor-pointer hover:bg-[rgba(226,226,234,0.7)] transition-all aspect-square border border-solid border-[rgba(226,226,234,0.7)] rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#B8B9BE"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="w-full">{children}</div>
+    </div>
+  );
+};
+
+export default ContactLayout;
+
+const HeadTab = ({ name, ico, active, setActive }) => {
+  return (
+    <button
+      onClick={() => setActive(name)}
+      className={`flex text-lg capitalize border-none bg-transparent justify-center cursor-pointer items-center gap-2 ${
+        active === name ? "text-[#0F6FDC] font-semibold " : "text-[#A0A0A0]"
+      } `}
+    >
+      {ico && ico}
+      {name}
+    </button>
+  );
+};
